ff75c2bdf4a8ab25b7020dda348092096a066f08
Prerna Das <dasp@ohsu.edu>
2019-11-05 11:29:56 -0800

    Update database_identifier.py

--- SRC/PYTHON/deduplicator/deduplicator/database_identifier.py
+++ SRC/PYTHON/deduplicator/deduplicator/database_identifier.py
@@ -1,3 +1,7 @@
+# Copyright 2019, University of Illinois at Chicago and Oregon Health & Science University
+# This file is part of the deduplicator project.
+# See the ReadMe.txt file for licensing information.
+
 def add_database_identifier(list_of_dicts):
 	"""
 	ID is source database: unique identifier for that database/accession number
